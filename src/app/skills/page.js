357d1f29b94d03f7b81324d2c60261923f5f31c9
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-11-17 21:28:58 +0300

    fix

--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -3,11 +3,11 @@ import { skills } from "@/data/Skills";
 const Skills = () => {
   return (
     <div className="container mx-auto">
-      <div className="grid xl:grid-cols-5 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10 md:p-0 p-3">
+      <div className="grid xl:grid-cols-5 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10 md:p-0 p-3 ">
         {skills.map((skill, index) => (
           <div
             key={index}
-            className=" bg-white p-3 h-[200px] flex flex-col items-center justify-center"
+            className=" bg-white p-3 h-[200px] flex flex-col items-center justify-center rounded-lg "
           >
             <img className="w-[100px]" src={skill.image} alt="skillImage" />
             <span className="pt-5 text-xl text-black">{skill.name}</span>

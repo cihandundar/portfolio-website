d63ec70339e3dec5c21c695ef82e01f361b4b1c5
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-11-17 21:35:28 +0300

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Hero from "@/components/hero/Hero";
 export default function Home() {
   return (
     <section>
-      <div className="container mx-auto flex flex-col items-center">
+      <div className="container mx-auto flex flex-col items-center md:p-0 p-3">
         <Hero />
         <Contact />
       </div>
